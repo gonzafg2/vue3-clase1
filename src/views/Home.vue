83b933c1018f3e5fdd8b65eacf546d5dd1069dcc
Gonzalo Fleming <gonzafg2@gmail.com>
2023-06-24 02:11:07 -0400

    Add vue router 4 and home view

---
+++ src/views/Home.vue
@@ -0,0 +1,85 @@
+<template>
+  <LoadingPage v-if="loading" />
+  <div class="container" v-else>
+    <div class="row text-center mt-3 mb-4">
+      <div class="col-12">
+        <TitlePage :colorClass="color" />
+      </div>
+      <div class="col-12">
+        <ButtonChangeColor @changeColorTextEmits="changeColorText" />
+      </div>
+    </div>
+    <div class="row">
+      <div v-for="post in postsPagination" class="col-4">
+        <CardPost
+          :titlePost="post.title"
+          :descriptionPost="post.body"
+          :idPost="post.id"
+        />
+      </div>
+    </div>
+    <PagePagination :data="postsAPI" @changePage="changePage"></PagePagination>
+  </div>
+</template>
+
+<script setup>
+import {
+  ref,
+  computed,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  onBeforeUpdate,
+  onBeforeMount,
+} from "vue";
+
+import TitlePage from "@/components/TitlePage.vue";
+import ButtonChangeColor from "@/components/ButtonChangeColor.vue";
+import CardPost from "@/components/CardPost.vue";
+import PagePagination from "@/components/PagePagination.vue";
+import LoadingPage from "@/components/LoadingPage.vue";
+
+const color = ref("color: black");
+const postsAPI = ref([]);
+const pageActual = ref(1);
+const loading = ref(true);
+
+const getPosts = (async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    postsAPI.value = data;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.value = false;
+  }
+})();
+
+const changeColorText = (colorEmits) => (color.value = colorEmits);
+
+const postsPagination = computed(() => {
+  const start = (pageActual.value - 1) * 12;
+  const end = start + 12;
+  return postsAPI.value.slice(start, end);
+});
+
+const changePage = (page) => (pageActual.value = page);
+
+// Lifecycle hooks
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+onMounted(() => {
+  console.log("onMounted");
+});
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate");
+});
+onUpdated(() => {
+  console.log("onUpdated");
+});
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
+</script>
