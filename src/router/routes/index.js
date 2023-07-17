79af4417877e3fe7ea100cfb9c60f24c3c92b3f2
Gonzalo Fleming <gonzafg2@gmail.com>
2023-07-17 13:51:32 -0400

    Add not found page

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -2,7 +2,12 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: () => import("@/views/home.vue"),
+    component: () => import("@/views/Home.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notFound",
+    component: () => import("@/views/404.vue"),
   },
 ];
 export default routes;
---
+++ src/views/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="d-flex justify-content-center h-vh-100">
+    <h1>Página no encontrada</h1>
+  </main>
+</template>
+
+<script setup></script>
